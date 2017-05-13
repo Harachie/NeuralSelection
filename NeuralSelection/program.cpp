562b3d45bb950abf768bcf46b18068fa5e77d932
Bastian Kröger <bastian.kroeger@outlook.com>
2017-05-13 11:57:45 +0200

    added data files added directory readout

--- NeuralSelection/program.cpp
+++ NeuralSelection/program.cpp
@@ -1,3 +1,16 @@
-int main() {
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string executablePath = argv[0];
+	string directory = executablePath.substr(0, executablePath.length() - 19); //-neuralselection.exe
+	string dataDirectory = directory + string("Data\\");
+	vector<string> stockDataFiles = { "ads.de.txt", "alv.de.txt", "bas.de.txt", "bayn.de.txt", "bei.de.txt", "bmw.de.txt", "cbk.de.txt", "dai.de.txt", "dbk.de.txt", "dpw.de.txt", "dte.de.txt", "eoan.de.txt", "fme.de.txt", "fre.de.txt", "hei.de.txt", "hen3.de.txt", "ifx.de.txt", "lha.de.txt", "lin.de.txt", "mrk.de.txt", "muv2.de.txt", "psm.de.txt", "rwe.de.txt", "sap.de.txt", "sie.de.txt", "tka.de.txt", "vow3.de.txt", "_con.de.txt" };
+
 	return 0;
 }
